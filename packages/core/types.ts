5149917bdd26e5159994adb4db8038ba218418b8
Renato Golia <renato@golia.tech>
2026-09-19 01:59:38 +0200

    Model collection reconciliation policy

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -2,6 +2,8 @@
 
 export type PropertyValue = string | boolean | readonly string[];
 
+export type CollectionReconciliationMode = "sparse" | "strict";
+
 export type BuiltInRepositoryPermission =
   | "pull"
   | "triage"
