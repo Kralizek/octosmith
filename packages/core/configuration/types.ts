f523a19aa39b58580168ed60679c4fdba2386376
Renato Golia <renato@golia.tech>
2026-09-19 02:04:59 +0200

    Format configuration types

--- packages/core/configuration/types.ts
+++ packages/core/configuration/types.ts
@@ -1,7 +1,7 @@
 import type {
+  CollectionReconciliationMode,
   MergeCommitMessage,
   MergeCommitTitle,
-  CollectionReconciliationMode,
   PropertyValue,
   RepositoryVisibility,
   RulesetBypassActorType,
