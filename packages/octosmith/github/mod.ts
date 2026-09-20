42a8e9330e98c4e2115ba693ea3aacf7cc8de846
Renato Golia <renato@golia.tech>
2026-09-20 18:01:28 +0200

    Move GitHub integration into OctoSmith package

---
+++ packages/octosmith/github/mod.ts
@@ -0,0 +1,7 @@
+export * from "./client.ts";
+export * from "./discovery.ts";
+export * from "./state_source.ts";
+export * from "./read_state.ts";
+export * from "./rest_state_source.ts";
+export * from "./apply.ts";
+export * from "./rest_mutation_sink.ts";
