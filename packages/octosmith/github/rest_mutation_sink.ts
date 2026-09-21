ce1ec2257590de283baec0579f048e26a7afcab9
Renato Golia <renato@golia.tech>
2026-09-22 01:17:43 +0200

    Use Octosmith branding in packages/octosmith/github/rest_mutation_sink.ts

--- packages/octosmith/github/rest_mutation_sink.ts
+++ packages/octosmith/github/rest_mutation_sink.ts
@@ -226,7 +226,7 @@ export class GitHubRepositoryMutationSink implements RepositoryMutationSink {
           this.repo(repository) + "/contents/" + encodePath(operation.path),
           {
             body: {
-              message: "OctoSmith: remove " + operation.path,
+              message: "Octosmith: remove " + operation.path,
               sha: operation.sha,
             },
           },
@@ -503,7 +503,7 @@ export class GitHubRepositoryMutationSink implements RepositoryMutationSink {
       this.repo(repository) + "/contents/" + encodePath(path),
       {
         body: {
-          message: "OctoSmith: apply " + path,
+          message: "Octosmith: apply " + path,
           content: encodeBase64(content),
           ...(sha !== undefined && { sha }),
         },
