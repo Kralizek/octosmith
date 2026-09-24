1b89bfd93bd32854fc5c6e2f5b56c2e95b9b50ba
Renato Golia <renato@golia.tech>
2026-09-24 14:49:48 +0200

    Package the complete schema tree

--- .github/workflows/scripts/sync-schemas.sh
+++ .github/workflows/scripts/sync-schemas.sh
@@ -6,5 +6,4 @@ target_dir="packages/octosmith/configuration/schemas"
 
 rm -rf "$target_dir"
 mkdir -p "$target_dir"
-cp "$source_dir/octosmith.schema.json" "$target_dir/octosmith.schema.json"
-cp "$source_dir/template.schema.json" "$target_dir/template.schema.json"
+cp -R "$source_dir"/. "$target_dir"/
